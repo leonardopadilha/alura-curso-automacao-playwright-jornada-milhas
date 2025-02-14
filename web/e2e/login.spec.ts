ccb7206069f387c8522efacfd4dc5c0b6f55f2d9
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-02-14 06:22:23 -0300

    Não realizar login com email inválido e senha válida

--- web/e2e/login.spec.ts
+++ web/e2e/login.spec.ts
@@ -16,4 +16,13 @@ test.describe('Página de Login', () => {
     await paginaLogin.fazerLogin('teste@teste.com', '1111');
     await paginaLogin.loginFeitoComSucesso('/home');
   });
+
+  test('Não deve conseguir fazer login com email inválido e senha válida', async ({ page }) => {
+
+    await paginaLogin.visitar('/');
+    await paginaLogin.clickLogin();
+    await paginaLogin.exibirLoginForm();
+    await paginaLogin.fazerLogin('teste.erro@teste.com', '1111');
+    await paginaLogin.mensagemLoginInvalido('Você não está autorizado a acessar este recurso');
+  });
 });
