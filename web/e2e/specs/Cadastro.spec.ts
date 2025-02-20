2686a2b0a563e2003ce84b53704305b436d1a969
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-02-20 07:55:57 -0300

    Cadastrando um novo usuário

--- web/e2e/specs/Cadastro.spec.ts
+++ web/e2e/specs/Cadastro.spec.ts
@@ -1,7 +1,27 @@
-import { test } from '../setup/fixtures';
+import { gerarPerfil } from "e2e/operacoes/gerarPerfil";
+import { test } from "e2e/setup/fixtures";
 
 test.describe('Página de cadastro', () => {
-  test("Deve realizar cadastro com sucesso", async ({ paginaCadastro }) => {
+  test("Deve conseguir fazer cadastro", async ({ paginaCadastro }) => {
     await paginaCadastro.visitar();
+
+    const novoUsuario = gerarPerfil();
+
+    await paginaCadastro.visitar();
+    await paginaCadastro.definirNome(novoUsuario.nome);
+    await paginaCadastro.definirDataNascimento(novoUsuario.dataNascimento);
+    await paginaCadastro.definirGenero(novoUsuario.genero);
+    await paginaCadastro.definirCPF(novoUsuario.cpf);
+    await paginaCadastro.definirTelefone(novoUsuario.telefone);
+    await paginaCadastro.definirCidade(novoUsuario.cidade);
+    await paginaCadastro.definirEstado(novoUsuario.estado);
+
+    await paginaCadastro.definirEmail(novoUsuario.email);
+    await paginaCadastro.confirmarEmail(novoUsuario.email);
+    await paginaCadastro.definirSenha(novoUsuario.senha);
+    await paginaCadastro.confirmarSenha(novoUsuario.senha);
+    await paginaCadastro.confirmarTermos();
+    await paginaCadastro.submeterForm();
+    await paginaCadastro.cadastroFeitoComSucesso();
   });
 });
