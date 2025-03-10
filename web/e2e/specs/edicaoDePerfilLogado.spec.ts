29612ec3c1e5a64065d5035566a8ef913aa68dee
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-03-10 07:23:04 -0300

    Cenário de edição do perfil

--- web/e2e/specs/edicaoDePerfilLogado.spec.ts
+++ web/e2e/specs/edicaoDePerfilLogado.spec.ts
@@ -1,7 +1,23 @@
+import { gerarPerfil } from "e2e/operacoes/gerarPerfil";
 import { testeLogado } from "e2e/setup/testeLogado";
 
 testeLogado.describe('Página de perfil', () => {
   testeLogado("Deve conseguir editar o perfil", async ({ paginaPerfil }) => {
     await paginaPerfil.visitar();
+
+    const novosDadosUsuario = gerarPerfil();
+    const emailAtual = await paginaPerfil.formBase.obterValorInputEmail();
+
+    await paginaPerfil.atualizarUsuario({ ...novosDadosUsuario, email: emailAtual });
+    await paginaPerfil.atualizadoComSucesso();
+
+    await paginaPerfil.visitar();
+    await paginaPerfil.dadosEstaoCorretos({ ...novosDadosUsuario, email: emailAtual });
+  });
+
+  testeLogado('Deve conseguir fazer logout', async ({ paginaPerfil }) => {
+    await paginaPerfil.visitar();
+    await paginaPerfil.deslogar();
+    await paginaPerfil.deslogadoComSucesso();
   });
 });
